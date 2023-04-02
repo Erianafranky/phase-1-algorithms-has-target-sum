bf58ca80689c3552678266612a0ef53902f59637
DorothyAriana <arianadottie@gmail.com>
2023-04-02 15:44:48 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,17 +1,46 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
-}
+  //iterate through each element
+  for (let i = 0; i < array.length; i++){
+    //for the current number identify a number that adds up to our target
+    const compNum = target - array[i]
+    //iterate through the remaining array
+    for (let j = i + 1; j < array.length; j++){
+      //return true if matching array found
+      if (array[j] === compNum){
+        return true;
+      }
+      }
+    }
+    //if we reach the end of the array and no matching number found return false
+    return false
+  }
+
 
 /* 
   Write the Big O time complexity of your function here
+  Given an input array of n elements, 
+  the worst case scenario is that the algorithm needs to make n² iterations.
+  This is expressed as O(n²) in big O notation
 */
 
 /* 
   Add your pseudocode here
+  look through each item in the array
+  caalculate the number we need to get the target sum
+  look through the rest of the array
+  check if it matches the target
+  return true if there is a matching element
+  otherwise return false
+
 */
 
 /*
   Add written explanation of your solution here
+  The function has an array of numbers and a target value
+  we should iterate through each element in the array and 
+  calculate the number that would need to be added to the 
+  element in the array and find out if its equal to the target value
 */
 
 // You can run `node index.js` to view these console logs
